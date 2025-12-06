571b0a8aaabdd0c138e4158000dbc844120ba53e
Shailesh8008 <shailesh10thd@gmail.com>
2025-12-06 18:41:21 +0530

    set cors origin

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ connectDB();
 const bcrypt = require("bcrypt");
 const userModel = require("./models/user");
 
-app.use(cors());
+app.use(cors({ origin: "https://shopbag0.netlify.app/" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", apiRouter);
